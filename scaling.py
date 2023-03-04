ed119bbd52f17d8958f4e8396960fbddb42e308d
briannechecketts <123423316+briannechecketts@users.noreply.github.com>
2023-03-04 14:07:01 -0500

    Update scaling.py

--- scaling.py
+++ scaling.py
@@ -2,8 +2,11 @@ f = open('datalist.txt','r')
 lines = f.readlines()
 
 nndatalist = []
+counter=0
 with open('ndatalist', 'w') as j:
     for i in lines:
-        j.write('python cutevent.py -i'+i )
+        i.replace('\n','')
+        print(i)
+        counter = +1
+        j.write('python cutevent.py -i'+i+'-o postcutevents' +str(counter)+'.root\n')
 print(j)
-#print(lines)
