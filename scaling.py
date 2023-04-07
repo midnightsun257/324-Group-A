3684c4637253850f3971c5b5855906d95bacefb9
briannechecketts <123423316+briannechecketts@users.noreply.github.com>
2023-04-07 12:52:13 -0400

    Update scaling.py

--- scaling.py
+++ scaling.py
@@ -1,8 +1,21 @@
+import argparse
+import os
+import shutil
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='Input')
+parser.add_argument('-o', '--output', help='Output')
+args = parser.parse_args()
+files = os.listdir(args.input)
+
 with open('datalist.txt', "r") as f:
     data= f.read().split()
-counter=0
-j= open('ndatalist.txt', "w")
-for i in data:
-    counter = counter +1
-    j.write('python cutevent.py -i'+i+'-o postcutevents' +str(counter)+'.root\n')
-#print(data)
+    counter=0
+    j= open('ndatalist.txt', "w")
+
+    for i in files:
+        counter = counter +1
+        j.write('python letsmakecuts.py -i'+ args.input + i + ' -o ' + args.output + 'postcutevents' + str(counter) +'.root\n')
+
+    f.close()
+    j.close()
